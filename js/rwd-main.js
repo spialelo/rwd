79b5b907b1f0efbf051450ea617cd2d8272e24a1
spialelo <ndphillips@gmail.com>
2015-02-16 02:09:37 -0500

    Carousel 4/5 working. Need to figure out container issue.

--- js/rwd-main.js
+++ js/rwd-main.js
@@ -7,5 +7,20 @@
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplaySpeed: 2000,
-      });
+      }),
+
+
+     $(function() {
+     var pgurl = window.location.href.substr(window.location.href
+.lastIndexOf("/")+1);
+     $("nav ul li a").each(function(){
+          if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+          $(this).addClass("active");
+     })
+})
+
+
+
     });
+
+
