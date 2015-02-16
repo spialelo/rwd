62680e045970c245670cc974ad4e105ed361d61b
spialelo <ndphillips@gmail.com>
2015-02-15 19:09:17 -0500

    Slick carousel used for responsive carousel

--- js/rwd-main.js
+++ js/rwd-main.js
@@ -1,1 +1,11 @@
 /*RWD TEST JavaScript*/
+
+ $(document).ready(function(){
+      $('.single-item').slick({
+        autoplay: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 2000,
+      });
+    });
